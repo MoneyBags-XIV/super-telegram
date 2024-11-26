16d0452b1701a1b0f4db4849fbd50b78d25ae54b
Isaac Lenzini <isaac.lenzini@gmail.com>
2024-11-25 19:46:20 -0500

    beginnings of structure

--- items.py
+++ items.py
@@ -1,2 +1,23 @@
-class Player:
+class Object:
+    def __init__(self, location, capacity=0, takable=False):
+        self.location = location
+        self.takable = takable
+        self.capacity = capacity
+    
+    def set_location(self, new_location):
+        index = self.location.contents.index(self)
+        del self.location.contents[index]
+        self.location = new_location
+        self.location.contents.append(self)
+
+
+class Item(Object):
+    pass
+
+
+class Room(Object):
+    pass
+
+
+class Player(Object):
     def __init__(self, location, contents, )

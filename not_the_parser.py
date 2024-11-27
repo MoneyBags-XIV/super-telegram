3aea437c539e4e7fd62023c5654a878d19c1a076
Isaac Lenzini <isaac.lenzini@gmail.com>
2024-11-27 11:59:07 -0500

    temp parser for testing

--- not_the_parser.py
+++ not_the_parser.py
@@ -1,11 +1,30 @@
 class Parser:
-    def __init__(self, game):
+    def __init__(self, player, game):
         self.game = game
+        self.player = player
     
     def parse(self, str):
 
+        verb = ''
+        direct = ''
+        indirect = ''
+
         if not str:
             print("Pardon?")
-            return
+            return verb, direct, indirect
         
-        return
+
+        str = str.split(' ')
+
+        verb = str[0]
+        verb = getattr(self.player, verb)
+
+        if len(str) > 1:
+            direct = str[1]
+            direct = self.game.find_object_by_name(direct)
+        
+        if len(str) > 2:
+            indirect = str[2]
+            indirect = self.game.find_object_by_name(indirect)
+        
+        return verb, direct, indirect

e25c4ae10a454331db811b4c682a1ddaa6fd9ec5
Isaac Lenzini <isaac.lenzini@gmail.com>
2024-11-27 11:59:35 -0500

    adds example container

--- test_game_file.py
+++ test_game_file.py
@@ -8,6 +8,7 @@ def create_game():
 
     game = Game("Test", 0)
     room = Room("Room", game, "This is a simple room.")
+    bag = Item("Nationals Bag", room, "You got this bag from going to NB Nationals. Don't let anyone see the relay patch.", capacity=5, takable=True)
     item = Item("Nasty Knife", room, "This knife is pretty gnarly! Never run with it.", takable=True)
 
     player = Player(room, 10, "You are looking magnificent today!")

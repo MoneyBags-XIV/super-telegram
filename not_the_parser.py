55a33f98dfc82547f2d1ba2bfefd65cb83edf653
Isaac Lenzini <isaac.lenzini@gmail.com>
2024-11-28 12:37:43 -0500

    starts writing actual parser

--- not_the_parser.py
+++ not_the_parser.py
@@ -1,30 +1,83 @@
 class Parser:
-    def __init__(self, player, game):
+    def __init__(self, player, game, verb_list):
         self.game = game
         self.player = player
+        self.verb_list = verb_list
     
-    def parse(self, str):
+    def parse(self):
 
-        verb = ''
-        direct = ''
-        indirect = ''
+        while True:
+            str = input("> ")
+            str = str.lower()
+            clean_chars = ['.', ',', '\'']
+            for x in clean_chars:
+                str = str.replace(x, '')
+            words = str.split(" ")
 
-        if not str:
-            print("Pardon?")
-            return verb, direct, indirect
+            verbs = []
+
+            #TODO find multiple objects with the same name and default to one in same room, or ask which one
+
+            for i in range(len(words)):
+                for j in range(i+1):
+                    ans = words[j:len(words)-i]
+                    ans = " ".join(ans)
+                    for x in self.verb_list:
+                        if ans in x.verbs:
+                            verbs.append(x)
+            
+            #TODO check for duplicates
+            
+            if not verbs:
+                print("There's no verb in that sentance!")
+                continue
+
+            if len(verbs) > 1:
+                print("There are too many verbs in that sentance!")
+                continue
+
+            verb = verbs[0]
+
+            #TODO allow verbs to take direct object, but not need one
+    
+            if not verb.expects_direct:
+                return verb.player_method, None, None
+            
+            nouns = []
+            
+            for i in range(len(words)):
+                for j in range(i+1):
+                    ans = words[j:len(words)-i]
+                    ans = " ".join(ans)
+                    if self.game.find_object_by_name(ans):
+                        nouns.append(ans)
+            
+            # if not self.accepts_multiple_direct and len(nouns) > 1:
+
+
+class Verb:
+    def __init__(self, verbs, player, expects_direct=False, expects_indirect=False, accepts_multiple_direct=False):
+        self.verbs = verbs
+        self.expects_direct = expects_direct
+        self.expects_indirect = expects_indirect
+        self.player_method = getattr(player, self.verbs[0])
+        self.accepts_multiple_direct = accepts_multiple_direct
+    
+    # def find_direct(self, words, game):
+    #     if not self.expects_direct:
+    #         return
+        
+    #     nouns = []
+        
+    #     for i in range(len(words)):
+    #         for j in i+1:
+    #             ans = words[j:len(words)-i]
+    #             ans = " ".join(ans)
+    #             if game.find_object_by_name(ans):
+    #                 nouns.append(ans)
+        
+    #     if not self.accepts_multiple_direct and len(nouns) > 1:
+            
+                
         
 
-        str = str.split(' ')
-
-        verb = str[0]
-        verb = getattr(self.player, verb)
-
-        if len(str) > 1:
-            direct = str[1]
-            direct = self.game.find_object_by_name(direct)
-        
-        if len(str) > 2:
-            indirect = str[2]
-            indirect = self.game.find_object_by_name(indirect)
-        
-        return verb, direct, indirect

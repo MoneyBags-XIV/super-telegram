ffeeb11fe9ee5b7412f0e2db56d5c60f2ca6181c
Isaac Lenzini <isaac.lenzini@gmail.com>
2024-12-05 17:40:42 -0500

    basic funtionality for noun parsing

--- not_the_parser.py
+++ not_the_parser.py
@@ -26,6 +26,7 @@ class Parser:
                     for x in self.verb_list:
                         if ans in x.verbs and not x in verbs:
                             verbs.append(x)
+                            used_verb = ans
                         
             if not verbs:
                 print("There's no verb in that sentence!")
@@ -37,36 +38,60 @@ class Parser:
 
             verb = verbs[0]
 
-            output = verb.parse_nouns(words)
+            output = verb.parse_nouns(words, used_verb)
 
 
 class Verb:
-    def __init__(self, verbs, player, expects_direct=False, needs_direct=False, accepts_multiple_direct=False, expects_indirect=False, needs_indirect=False):
+    def __init__(self, verbs, player, expects_direct=False, needs_direct=False, accepts_multiple_direct=False, needs_indirect=False):
         self.verbs = verbs
 
         self.expects_direct = expects_direct
         self.needs_direct = needs_direct
         self.accepts_multiple_direct = accepts_multiple_direct
 
-        self.expects_indirect = expects_indirect
         self.needs_indirect = needs_indirect
 
         self.player_method = getattr(player, self.verbs[0])
         self.game = self.player.game
     
-    def parse_nouns(self, words):
+    def parse_nouns(self, words, used_verb):
 
         direct = None
         indirect = None
 
         if not self.expects_direct:
-            return direct, indirect
+            return (self.player_method, direct, indirect)
         
-        indirect = self.get_indirect(words)
+        indirect, words = self.get_indirect(words)
 
-        if indirect and not self.expects_indirect:
+        if indirect and not self.needs_indirect:
             return "That sentence doesn't make sense."
         
+        direct = self.get_direct(words)
+
+        if not direct and self.needs_direct:
+            ans = input("What do you want to " + used_verb + "?")
+            direct = self.find_nouns(ans)
+            if not direct:
+                return "I don't understand what you want to do."
+        
+        if len(direct) > 0 and not self.accepts_multiple_direct:
+            return "You can't use multiple direct objects with the verb: \"" + used_verb + ".\""
+        
+        if not self.needs_indirect:
+            return (self.player_method, direct, indirect)
+        
+        if not indirect:
+            ans = input("What do you want to " + used_verb + " the " + direct + " with?")
+            indirect = self.find_nouns(ans)
+            if not indirect:
+                return "Good luck with that!"
+        
+        if len(indirect) > 1:
+            return "You can't do that."
+        
+        return (self.player_method, direct, indirect)
+
         
 
     def get_direct(self, words):
@@ -75,7 +100,13 @@ class Verb:
     def get_indirect(self, words):
         pass
 
-    def find_nouns(self, words):
+    def find_nouns(self, str):
+        str = str.lower()
+        clean_chars = ['.', ',', '\'']
+        for x in clean_chars:
+            str = str.replace(x, '')
+        words = str.split(" ")
+
         nouns = []
 
         for i in range(len(words)):
@@ -87,3 +118,5 @@ class Verb:
 
                 if x:
                     nouns.append(x)
+        
+        return nouns
